8641793f2baace29f15b57f796d1b1b94d1474e0
wang20000 <2643509174@qq.com>
2022-08-01 11:07:57 +0800

    'tabbar'

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/',
   publicPath: '/',
   history: {
-    type: 'browser'
+    type: 'browser',
   },
   access: {},
   model: {},
@@ -14,21 +14,23 @@ export default defineConfig({
   initialState: {},
   proxy: {
     '/api': {
-      'target': 'https://www.kuaikanmanhua.com/',
-      'changeOrigin': true,
-      'pathRewrite': { '^/api': '' },
-    }
+      target: 'https://www.kuaikanmanhua.com/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
   },
   request: {
-    dataField: 'data'
+    dataField: 'data',
   },
   routes: [
-    { path: "/", component: "home", title: "首页" },
-    { path: "/renew", component: "renew", title: "更新" },
-    { path: "/rank", component: "rank", title: "排行榜" },
-    { path: "/sort", component: "sort", title: "分类" },
-    { path: "/chapter", component: "chapter", title: "章节" },
-    { path: "/content", component: "content", title: "章节内容" },
+    { path: '/', component: 'home', title: '首页' },
+    { path: '/renew', component: 'renew', title: '更新' },
+    { path: '/rank', component: 'rank', title: '排行榜' },
+    { path: '/sort', component: 'sort', title: '分类' },
+    { path: '/chapter', component: 'chapter', title: '章节' },
+    { path: '/content', component: 'content', title: '章节内容' },
+    { path: '/my', component: 'my', title: '我的' },
+    { path: '/search', component: 'search', title: '搜索' },
   ],
   npmClient: 'pnpm',
 });
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={styles.title}>搜索页面</h1>
+    </div>
+  );
+}
