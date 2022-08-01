cad0558b84adb9294306f34388e45b3763c47a9b
vjdsbw <1459281431@qq.com>
2022-08-01 23:11:18 +0800

    '世界页面'

--- src/services/user.ts
+++ src/services/user.ts
@@ -33,7 +33,11 @@ export function searchsort(tag:number,sort:number,update_status:number) {
     return request(`/api/v1/search/by_tag?since=0&count=24&f=3&tag=${tag}&sort=${sort}&query_category={"update_status":${update_status}}`)
 }
 
-//获取漫画每一章内容
+//获取漫画每一章内容 /v1/graph/pc/feeds/getRecommendFeed?uid=0&webTokenId=1615007958330_FFwnyURnzD0rgO2&since=0&limit=20
 export function queryContent(id:string) {
     return request(`/api/v2/pweb/comic/${id}`)
+}
+
+export function queryWorld() {
+    return request('/api/v1/graph/pc/feeds/getRecommendFeed?uid=0&webTokenId=1615007958330_FFwnyURnzD0rgO2&since=0&limit=10')
 }
