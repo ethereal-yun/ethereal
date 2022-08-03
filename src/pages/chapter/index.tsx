4b8dc1c44d6c2d98fd89a29b5ccc0303f5835a79
yyq1004 <578160208@qq.com>
2022-08-03 15:33:06 +0800

    tabbar

--- src/pages/chapter/index.tsx
+++ src/pages/chapter/index.tsx
@@ -1,63 +1,165 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.less';
 import { getChapter } from '@/services/user';
-import { useRequest } from '@umijs/max';
-import { Tabs,Space } from 'antd-mobile'
-import { StarOutline } from 'antd-mobile-icons'
+import { Tabs, Toast, Button, Popup, Space, TextArea, Form, Dialog, Input, ImageViewer, Card } from 'antd-mobile'
+import { StarOutline, UpOutline, DownOutline } from 'antd-mobile-icons'
+import { useSearchParams, useNavigate, useRequest } from '@umijs/max';
+import ImageUploader, { ImageUploadItem } from 'antd-mobile/es/components/image-uploader';
+import { mockUpload } from './utils';
+import { connect } from '@umijs/max';
 
-export default function Page() {
-  const { data } = useRequest(() => getChapter(1749));
-  // console.log(data);
+const Page = ({ elist, cLists, dispatch }) => {
+  const [SearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const { data: cdata, loading } = useRequest(() => getChapter(Number(SearchParams.get('id')!)), { cacheKey: 'chapter' });
+  const [visible1, setVisible1] = useState(false);
+  const [fileList, setFileList] = useState<ImageUploadItem[]>([{ url: '' },]);
+  const [Lists, setLists] = useState([]) as any
+  const onFinish = (values: any) => {
+    values.id = cdata.topic_info.id;
+    dispatch({
+      type: "evaluate/save",
+      payload: { values }
+    })
+    Dialog.alert({
+      content: "评论成功",
+      closeOnMaskClick: true,
+    })
+    setVisible1(false);
+  }
+  const GoContent = (need: boolean, id: string, title: string) => {
+    if (need) {
+      Toast.show({
+        content: '当前是vip章节,升级会员后方可解锁观看',
+        maskClickable: false,
+        duration: 2000,
+      })
+    } else {
+      navigate(`/content/${id}?title=${title}`)
+    }
+  }
 
+  useEffect(() => {
+    cdata && setLists([...cdata.topic_info.comics])
+  }, [cdata])
 
+  useEffect(() => {
+    if (!Lists) return;
+    dispatch({
+      type: "chaplist/save",
+      payload: Lists
+    })
+  }, [Lists])
   return (
     <div>
-       <img className={styles.maximg} src={data && data.topic_info.cover_image_url} />
+      <img className={styles.maximg} src={cdata && cdata.topic_info.cover_image_url} />
       <div className={styles.detail}>
         <div className={styles.pic}>
-            <img className={styles.minimg} src={data && data.topic_info.vertical_image_url} />
-          </div>
-
-          <div className={styles.detright}>
-          <h2 className={styles.title}>{data && data.topic_info.title}</h2>
-          <p className={styles.descript}>作者：{data && data.topic_info.user.nickname}</p>
-          <p>人气：{data && data.topic_info.popularity_info}</p>
-          <p>类型：{data && data.topic_info.tags.map((item:any,index:number)=>{
+          <img className={styles.minimg} src={cdata && cdata.topic_info.vertical_image_url} />
+        </div>
+        <div className={styles.detright}>
+          <h2 className={styles.title}>{cdata && cdata.topic_info.title}</h2>
+          <p className={styles.descript}>作者：{cdata && cdata.topic_info.user.nickname}</p>
+          <p>人气：{cdata && cdata.topic_info.popularity_info}</p>
+          <p>类型：{cdata && cdata.topic_info.tags.map((item: any, index: number) => {
             return <span key={index}>{item}</span>
           })}</p>
-           <div className={styles.collect}>
-          收藏
-        <StarOutline className={styles.col} />
+          <div className={styles.collect}>
+            收藏
+            <StarOutline className={styles.col} />
           </div>
         </div>
-        </div>
-
-
-        <div className={styles.intro}>
+      </div>
+      <div className={styles.intro}>
         <h2 className={styles.introtit}>漫画简介</h2>
-        <span>{data && data.topic_info.description}</span>
-        </div>
+        <span>{cdata && cdata.topic_info.description}</span>
+      </div>
 
-        <Tabs>
-          <Tabs.Tab title='章节列表' key='lists'>
+      {/* 章节排序 */}
+      <div className={styles.sort}>
+        <Button color='success' size='mini' onClick={() => { setLists([...Lists.reverse()]) }}>切换顺序</Button>
+      </div>
+
+      <Tabs>
+        <Tabs.Tab title='章节列表' key='lists'>
           {
-           data && data.topic_info.comics.map((item:any,index:number)=>{
-             return <div key={index} className={styles.listitem}>
-               <div>
-               <img className={styles.listimg} src={item.cover_image_url} />
-               </div>
-               <div>
-               <h3 className={styles.listitle}>{item.title}</h3>
-               <p className={styles.data}>{item.created_at}</p>
-               </div>
-               </div>
-             })
+            cLists && cLists.map((item: any, index2: number) => {
+              return <div key={index2} className={styles.listitem} onClick={() => GoContent(item.need_vip, item.id, item.title)}>
+                <div>
+                  <img className={styles.listimg} src={item.cover_image_url} />
+                </div>
+                <div>
+                  <h3 className={styles.listitle}>{item.title}</h3>
+                  <p className={styles.data}><span className={styles.span}>{item.need_vip ? item.label_info.text : ""}</span>{item.created_at}</p>
+                </div>
+              </div>
+            })
           }
-          </Tabs.Tab>
-          <Tabs.Tab title='评价留言' key='message'>
-            暂无评价
-          </Tabs.Tab>
-        </Tabs>
+        </Tabs.Tab>
+        <Tabs.Tab title='评价留言' key='message'>
+          <Button block color='primary' size='large' onClick={() => { setVisible1(true) }} className={styles.evaluate}>
+            去评价
+          </Button>
+          <Space direction='vertical'>
+            <Popup visible={visible1} onMaskClick={() => { setVisible1(false) }} bodyStyle={{ minHeight: '40vh' }}>
+              <Form onFinish={onFinish} footer={<Button block type='submit' color='primary' size='large'>提交</Button>}>
+                <Form.Item name='text'>
+                  <TextArea defaultValue={''} showCount />
+                </Form.Item>
+                <Form.Item name='id'>
+                </Form.Item>
+                <Form.Item name='url'>
+                  <ImageUploader value={fileList} onChange={setFileList} upload={mockUpload} />
+                </Form.Item>
+              </Form>
+            </Popup>
+          </Space>
+        </Tabs.Tab>
+      </Tabs>
+      {elist && elist.map((item: any, index: number) => {
+        if (item.id == cdata.topic_info.id) {
+          return <div key={index}>
+             <Card>
+              <div className={styles.content}>内容：{item.text}</div>
+              {
+                item.url && item.url.map((item2: any, index1: number) => {
+                  return <span className={styles.image} onClick={() => { setVisible1(true) }} key={index1}>
+                    <img src={item2.url} className={styles.imagess}></img>
+                  </span>
+                })
+              }
+            </Card>
+          </div>
+        }
+
+      })
+
+
+      }
+      {/* {
+        elist && elist.map((item: any,index:number) => {
+          return <div key={index}>
+            {item}
+            <Card>
+              <div className={styles.content}>内容：{item.values.text}</div>
+              {
+                item && item.values.url.map((item2: any, index1: number) => {
+                  return <span className={styles.image} onClick={() => { setVisible1(true) }} key={index1}>
+                    <img src={item2.url} className={styles.imagess}></img>
+                  </span>
+                })
+              }
+            </Card>
+          </div>
+        })
+      } */}
     </div>
   );
 }
+
+
+export default connect(({ evaluate, chaplist }) => ({
+  elist: evaluate.elist,
+  cLists: chaplist.cLists
+}))(Page);
+
--- src/pages/renew/index.tsx
+++ src/pages/renew/index.tsx
@@ -1,42 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import styles from './index.less';
 import { getRenew } from '@/services/user';
-import { useRequest } from '@umijs/max';
+import { useRequest,useNavigate } from '@umijs/max';
 import { Tabs,Space,Tag } from 'antd-mobile'
 
 export default function Page() {
   let [day,setDay]=useState(6);
+  const navigate = useNavigate();
 
-  const { data,run,refresh } = useRequest(() => getRenew({
+  const { data:rdata,run,refresh } = useRequest(() => getRenew({
     pos:day
   }),{
-    manual: true,
-    refreshDeps: [day],
+    manual: true
   });
 
   const callback=(key:string)=>{
     let count;
-    console.log('onChange', key);
+    // console.log('onChange', key);
     count=Number(key)
     setDay(count)
-    console.log(count,"xxxx") 
+    day = count;
+    // console.log(count,"xxxx") 
+    refresh()
   }
 
-  useEffect(()=>{ 
-    let now = new Date();
-    var dd = now.getDay(); 
-    setDay[dd]   
+  const goContent = (id:string,title:string) => {
+       navigate(`/chapter/?title=${title}&id=${id}`)
+  }
+
+
+  useEffect(()=>{   
     run()
-    refresh()
-  },[day,data])
+  },[])
   
   return (
     <div>
-        <Tabs defaultActiveKey='1' onChange={callback} >
-          <Tabs.Tab title='周一' key='0' >
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
+        <Tabs defaultActiveKey='0' onChange={callback} >
+           {
+             rdata && rdata.week_days.map((item:any,index:number)=>{
+               return <Tabs.Tab title={item} key={index}>
+                 {
+              rdata && rdata.topics.map((item:any,index:number)=> {
+                return <div key={index} onClick={()=>goContent(item.id,item.title)}>
                   <div className='title'>
                   <Space>
                    <Tag color='danger'>{item.tags}</Tag>
@@ -48,117 +53,11 @@ export default function Page() {
                   <img src={item.cover_image_url} className={styles.renewimg} />
                   </div>
                   </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周二' key='1'>
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周三' key='2'>
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周四' key='3'>
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周五' key='4'>
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周六' key='5'>
-            {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
-          <Tabs.Tab title='周日' key='6'>
-          {
-              data && data.topics.map((item:any,index:number)=> {
-                return <div key={index}>
-                  <div className='title'>
-                  <Space>
-                   <Tag color='danger'>{item.tags}</Tag>
-                   <span className={styles.rnewtitle}>{item.title}</span>
-                  </Space>
-                  <p className={styles.author}>作者：{item.user.nickname}</p>
-                  </div>
-                  <div className={styles.img}>
-                  <img src={item.cover_image_url} className={styles.renewimg} />
-                  </div>
-                  </div>
-              })
-            }
-          </Tabs.Tab>
+                 })
+               }
+               </Tabs.Tab>
+             })
+           }
         </Tabs>
     </div>
   );
